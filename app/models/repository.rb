04267a2fcd7fb75d24c7c6ac208776df4e954af9
dmitriy <d.dobrenkiy@hotmail.com>
2022-02-17 19:31:22 +0300

    enumerize language

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class Repository < ApplicationRecord
+  extend Enumerize
+
   belongs_to :user
+  enumerize :language, in: [:JavaScript]
+  validates :language, presence: true
 end
