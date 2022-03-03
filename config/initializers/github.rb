275287a28dfdb82f7c91d652167d7857ae0082eb
Dmitriy <reg.dobr@live.ru>
2022-03-03 14:07:42 +0300

    fix: github scopes

--- config/initializers/github.rb
+++ config/initializers/github.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_APP_ID'], ENV['GITHUB_APP_SECRET'], scope: 'user:email'
+  provider :github, ENV['GITHUB_APP_ID'], ENV['GITHUB_APP_SECRET'], scope: 'user,repo,admin:repo_hook'
 end
