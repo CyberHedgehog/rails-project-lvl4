3bed5fc97d47524cf4bbb23f9665ed0ced4e536b
Dmitriy <reg.dobr@live.ru>
2022-03-09 15:25:55 +0300

    feat: api_checks test

---
+++ test/controllers/api/checks_controller_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class Api::ChecksControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    repo = repositories(:one)
+    @repo_commits = JSON.parse(load_fixture('files/commits.json'))
+    stub_request(:get, "https://api.github.com/repos/#{repo.full_name}/commits").to_return(status: 200, body: @repo_commits)
+  end
+
+  test 'hook_test' do
+    hook_commits = JSON.parse(load_fixture('files/api/commits.json'))
+
+    post api_checks_path hook_commits
+    check = Repository::Check.find_by(commit: hook_commits['commits'][0]['id'])
+    assert check
+  end
+end
