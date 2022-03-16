7a750160f423976409cf2ad6614469cf5598e3fa
dmitriy <d.dobrenkiy@hotmail.com>
2022-03-16 21:00:26 +0300

    fix: auth test

--- test/controllers/auth_controller_test.rb
+++ test/controllers/auth_controller_test.rb
@@ -4,9 +4,15 @@ require 'test_helper'
 
 class AuthControllerTest < ActionDispatch::IntegrationTest
   test 'should sign in' do
-    user = users(:one)
-    sign_in(user)
-    assert session[:user_id]
+    github_user_data = Faker::Omniauth.github
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:github] = nil
+    OmniAuth.config.add_mock(:github, github_user_data)
+    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:github]
+    get callback_auth_path :github
+    user = User.find_by(email: github_user_data[:info][:email])
     assert_redirected_to root_path
+    assert session[:user_id]
+    assert user
   end
 end
