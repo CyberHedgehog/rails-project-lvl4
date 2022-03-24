c7721f0f40d23f4ea2cf80c1723d0045edda97ac
Dmitriy <reg.dobr@live.ru>
2022-03-24 14:07:36 +0300

    fix: check job

--- app/jobs/check_repository_job.rb
+++ app/jobs/check_repository_job.rb
@@ -7,9 +7,9 @@ class CheckRepositoryJob < ApplicationJob
     check.check!
     checker = ApplicationContainer['check_repository_service'].new
     client = Octokit::Client.new
-    repo_data = client.repository(check.repository.full_name)
+    # repo_data = client.repository(check.repository.full_name)
     commit_name = client.commits(check.repository.full_name).first[:sha]
-    checker.download(repo_data.clone_url)
+    checker.download("https://github.com/#{check.repository.full_name}.git")
     check_result, code = checker.check(check.repository.language)
     check.update(commit: commit_name, result: check_result, passed: code.success?)
     RepositoryCheckMailer.with(check: check).report_failed_check.deliver_later unless code.success?
