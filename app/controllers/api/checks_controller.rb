20f16691fd807a1aa3d9ffe1fe1c9a223b00145a
dmitriy <d.dobrenkiy@hotmail.com>
2022-03-30 22:25:49 +0300

    fix api_checks

--- app/controllers/api/checks_controller.rb
+++ app/controllers/api/checks_controller.rb
@@ -4,7 +4,11 @@ class Api::ChecksController < Api::ApplicationController
   skip_before_action :verify_authenticity_token
   def create
     repository = Repository.find_by(github_id: params['repository']['id'])
-    render json: { error: t('api.check.repository_not_found') } if repository.nil?
+    if repository.nil?
+      render json: { error: t('api.check.repository_not_found') }
+      return
+    end
+
     check = repository.checks.new(commit: '')
     if check.save
       CheckRepositoryJob.perform_later(check)
