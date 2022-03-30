1b4b7b3071cf834b75ae30de20adcebc460b4825
dmitriy <d.dobrenkiy@hotmail.com>
2022-03-30 22:12:58 +0300

    fix api_checks

--- app/controllers/api/checks_controller.rb
+++ app/controllers/api/checks_controller.rb
@@ -3,11 +3,14 @@
 class Api::ChecksController < Api::ApplicationController
   skip_before_action :verify_authenticity_token
   def create
-    render json: { error: 'Bad request data' } if params['commits'].nil?
-
     repository = Repository.find_by(github_id: params['repository']['id'])
-    check = repository.checks.create(commit: params['commits'][0]['id'])
-    CheckRepositoryJob.perform_later(check)
-    render json: check.to_json
+    render json: { error: t('api.check.repository_not_found') } if repository.nil?
+    check = repository.checks.new(commit: '')
+    if check.save
+      CheckRepositoryJob.perform_later(check)
+      render json: check.to_json
+    else
+      render json: { error: t('api.check.create.error') }
+    end
   end
 end
