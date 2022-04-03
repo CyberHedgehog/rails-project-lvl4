dd7a5512cb4df3a528b4bff995376c7d6a780726
dmitriy <d.dobrenkiy@hotmail.com>
2022-04-03 22:56:08 +0300

    test: check_repository_job

--- app/controllers/api/checks_controller.rb
+++ app/controllers/api/checks_controller.rb
@@ -5,16 +5,19 @@ class Api::ChecksController < Api::ApplicationController
   def create
     repository = Repository.find_by(github_id: params['repository']['id'])
     if repository.nil?
+      pp 'repository not found', repository
       render json: { error: t('api.check.repository_not_found') }
       return
     end
 
     check = repository.checks.new(commit: '', passed: false)
     if check.save
+      pp 'check before job', check
       CheckRepositoryJob.perform_later(check)
-      pp '!!!!!Check', check
+      pp 'check after job', check
       render json: check.to_json
     else
+      pp 'not created'
       render json: { error: t('api.check.create.error') }
     end
   end
