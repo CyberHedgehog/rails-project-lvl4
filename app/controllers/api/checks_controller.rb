5cb84e275a61b32d52c12cb10d589492f7df704c
dmitriy <d.dobrenkiy@hotmail.com>
2022-04-03 23:09:17 +0300

    removed console prints

--- app/controllers/api/checks_controller.rb
+++ app/controllers/api/checks_controller.rb
@@ -5,19 +5,15 @@ class Api::ChecksController < Api::ApplicationController
   def create
     repository = Repository.find_by(full_name: params['repository']['full_name'])
     if repository.nil?
-      pp 'repository not found', repository
       render json: { error: t('api.check.repository_not_found') }
       return
     end
 
     check = repository.checks.new(commit: '', passed: false)
     if check.save
-      pp 'check before job', check
       CheckRepositoryJob.perform_later(check)
-      pp 'check after job', check
       render json: check.to_json
     else
-      pp 'not created'
       render json: { error: t('api.check.create.error') }
     end
   end
