90c3732ef53d66e8f22b501eba15651677eee653
Dmitriy <reg.dobr@live.ru>
2022-04-08 11:56:25 +0300

    fix: codestyle

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-Rails.application.routes.draw do  
+Rails.application.routes.draw do
   scope module: :web do
     post '/auth/:provider', to: 'auth#request', as: :auth_request
     get '/auth/:provider/callback', to: 'auth#callback', as: :callback_auth
