7319497683066530d074a929c23636b8d3d18c3e
Dmitriy <reg.dobr@live.ru>
2022-04-08 11:55:37 +0300

    auth routes moved to web

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
-Rails.application.routes.draw do
-  post '/auth/:provider', to: 'auth#request', as: :auth_request
-  get '/auth/:provider/callback', to: 'web/auth#callback', as: :callback_auth
-
+Rails.application.routes.draw do  
   scope module: :web do
+    post '/auth/:provider', to: 'auth#request', as: :auth_request
+    get '/auth/:provider/callback', to: 'auth#callback', as: :callback_auth
     resources :sessions, only: :destroy
     root to: 'home#index'
     resources :repositories, only: %i[index show new create] do
